6f2ee6f9443c91165d57f57f7f271f86a3020134
Paul Yim <paul.j.yim@gmail.com>
2021-02-14 22:27:15 +0000

    Add bootstrapping template; move deps under bootstrapping template

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+fi
+
 ./configure --prefix="${PREFIX}"
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
